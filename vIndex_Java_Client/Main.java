0d1bd2cffaba3a6cbf596f638905a000088026ad
Obi leppy <return0000000@gmail.com>
2020-10-18 17:51:32 +0200

    Added Network socket instance

--- vIndex_Java_Client/Main.java
+++ vIndex_Java_Client/Main.java
@@ -1,8 +1,11 @@
-
+import src.Networking.Network_Socket;
+import src.Networking.Network_Socket;
 public class Main {
     public static void main(String[] args){
 
         //new Frame_00();
+
+        Network_Socket nSocket = new Network_Socket("localhost", 50000);
         
 
     }
