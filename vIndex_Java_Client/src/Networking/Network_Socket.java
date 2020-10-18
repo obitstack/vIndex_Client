7495543d151d1189a3ad1e36363d56a2898357d4
Obi leppy <return0000000@gmail.com>
2020-10-18 18:13:10 +0200

    Test Local host connection

--- vIndex_Java_Client/src/Networking/Network_Socket.java
+++ vIndex_Java_Client/src/Networking/Network_Socket.java
@@ -4,11 +4,12 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Network_Socket {
 
     private int nPort; // networking port
-    private String nAdress; // networkig adress
+    private String nAdress; // networking adress
 
     public Network_Socket(String network_adress, int port_number){
         this.nAdress = network_adress;
@@ -17,16 +18,24 @@ public class Network_Socket {
         try (Socket socket = new Socket(nAdress, nPort)) {
 
             System.out.println("Connected to server"); 
-            BufferedReader input =  new BufferedReader( new InputStreamReader(socket.getInputStream()));  
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);  
-            
-            while(true){
-                String echoString = input.readLine();
-                if(echoString.equals("exit")){
-                    break;
+            BufferedReader echoes =  new BufferedReader( new InputStreamReader(socket.getInputStream()));  
+            PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);  
+
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+            String response;
+
+            do {
+                System.out.println("Enter string to be Echoed: ");
+                echoString = scanner.nextLine();
+                stringToEcho.println(echoString);
+                if(!echoString.equals("exit")){
+                    response = echoes.readLine();
+                    System.out.println(response);
                 }
-                output.println("Echo from server" + echoString);
-            }
+                
+            } while (!echoString.equals("exit"));
+            
         } catch (Exception e) {
             System.out.println("Client error: " + e.getMessage());
             e.printStackTrace();
